7fdf3433ea53c67ffbb49a4402f9f0328fcf72f5
Tanmayop9 <102711021+Tanmayop9@users.noreply.github.com>
2025-12-11 17:34:35 +0530

    Revert "Migrate database from JoshDB to MongoDB"

--- src/functions/josh.js
+++
@@ -1,8 +0,0 @@
-/**
- * MongoDB wrapper with JoshDB-compatible API
- * Migrated from @joshdb to MongoDB for better scalability
- */
-
-export { josh } from './mongoWrapper.js';
-
-/**@codeStyle - https://google.github.io/styleguide/tsguide.html */
